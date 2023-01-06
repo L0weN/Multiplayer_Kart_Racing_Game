ec243385c7ac00b8432fd0ccf3c56a90997ff6cb
Mert <mertozata009@gmail.com>
2023-01-06 20:17:42 +0300

    Multiplayer_Kart_Racing_Game.V1.1
    Driving mechanics

--- Assets/Scripts/Drive.cs
+++ Assets/Scripts/Drive.cs
@@ -9,6 +9,8 @@ public class Drive : MonoBehaviour
     public GameObject[] Wheels;
     public float torque = 200;
     public float maxSteerAngle = 30;
+    public float maxBrakeTorque = 500;
+    
     void Start()
     {
     }
@@ -17,22 +19,27 @@ public class Drive : MonoBehaviour
     {
         float a = Input.GetAxis("Vertical");
         float s = Input.GetAxis("Horizontal");
-        Go(a,s);
+        float b = Input.GetAxis("Jump");
+
+        Go(a,s,b);
     }
 
-    void Go(float accel, float steer)
+    void Go(float accel, float steer, float brake)
     {
         accel = Mathf.Clamp(accel, -1, 1);
         steer = Mathf.Clamp(steer, -1, 1) * maxSteerAngle;
-        
+        brake = Mathf.Clamp(brake, 0, 1) * maxBrakeTorque;
+
         float thrustTorque = accel * torque;
+        
         for (int i = 0; i < 4; i++)
         {
             WC[i].motorTorque = thrustTorque;
 
             if (i < 2)
                 WC[i].steerAngle = steer;
-
+            else
+                WC[i].brakeTorque = brake;
             Quaternion quat;
             Vector3 position;
 
